1553300339dca046f44eb0edc76db21e53547292
sabrinapct <45568186+sabrinapct@users.noreply.github.com>
2018-12-05 18:28:13 +0100

    Update test.java

--- test.java
+++ test.java
@@ -1,11 +1,11 @@
-import java.util.Scanner;
+import java.util.Scanner;//J'importe le scanner pour répondre a mon operateur
 
-class test {
-public static void main(String[]arg) {
-	Scanner sc = new Scanner(System.in);
-        System.out.println("Quel est ton nom");
-        String str = sc.nextLine();  
-        System.out.println("Coucou" + " " + str) ;
+class test {//J'importe ma classe 
+public static void main(String[]arg) {//Je définis la méthode
+	Scanner sc = new Scanner(System.in);//Je déclare le scanner
+        System.out.println("Quel est ton nom");//J'imprime la question de sortie
+        String str = sc.nextLine();  //Je crée une chaine pour lui ajouter une réponse
+        System.out.println("Coucou" + " " + str) ;// J'imprime "coucou" en réponse à la sienne 
           
 }
 }
