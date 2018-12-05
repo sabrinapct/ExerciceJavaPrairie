52f09bf783c629a0566d74ec9994b1607c7b2a15
sabrinapct <45568186+sabrinapct@users.noreply.github.com>
2018-12-05 18:20:49 +0100

    Update TableMulti.java

--- TableMulti.java
+++ TableMulti.java
@@ -1,13 +1,13 @@
 package ExerciceMulti;//J'appel le package
 
-public class TableMulti {// je declare la class
+public class TableMulti {// J'appel ma class
 
-                public static void main(String[] args) {
+                public static void main(String[] args) {//Je définis la méthode
 
-		        int num = 3;// Je définis ma variable
+		        int num = 3;// J'atribue a ma variable une valeur
 		        for(int i = 1; i <= 10; ++i)// je définis la formule de boucle
 		        {
-		            System.out.printf("%d * %d = %d \n", num, i, num * i);//Je définis la formule de sortie
+		            System.out.printf("%d * %d = %d \n", num, i, num * i);//Je définis le calcul de sortie
 		        }
 		    }
 		
