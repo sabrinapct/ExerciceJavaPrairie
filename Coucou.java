fcdf09f03252f9c97ce2c9c31c8517eee47789d4
sabrinapct <45568186+sabrinapct@users.noreply.github.com>
2018-12-05 18:17:44 +0100

    Update Coucou.java

--- Coucou.java
+++ Coucou.java
@@ -1,5 +1,5 @@
-public class Coucou {
-public static void main(String[]arg) {
-        System.out.println("coucou tout le monde");
-}
-}
+public class Coucou {//J'appel la class
+public static void main(String[]arg) {//Je définis la méthode
+        System.out.println("coucou tout le monde");// J'imprime la phrase
+} //Je cloture ma chaine
+}// Je cloture ma commande
